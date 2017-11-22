70c99a512fbab2a7e34c9aebe74224e80f39c832
Seth <seth@eventespresso.com>
2017-11-22 08:47:59 -0700

    Adding the ee_bb_suppress_notices function

--- functions.php
+++ functions.php
@@ -9,3 +9,12 @@ require_once 'classes/class-fl-child-theme.php';
 
 // Actions
 add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+
+if (!function_exists('ee_bb_suppress_notices')){
+	function ee_bb_suppress_notices(){
+	    if (class_exists('FLBuilderModel') && (FLBuilderModel::is_builder_enabled())){
+	                add_filter( 'FHEE__EE_Front_Controller__display_errors', '__return_false' );
+	    }
+	}
+	add_action('wp', 'ee_bb_suppress_notices', 1);
+}
